54cc3c86f87a70c633e02f8c035891ccd8bbbde4
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-10-10 22:16:26 +0600

    Добавьте файлы проекта.

---
+++ Lab7_Task1/Lab7_Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны первого треугольника");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            double S1;
+            CulcArea(a1, b1, c1, out S1);
+            Console.WriteLine("Площадь первого треугольника равна: {0:##########.00}",S1);
+
+            Console.WriteLine("Введите стороны второго треугольника");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int c2 = Convert.ToInt32(Console.ReadLine());
+            double S2;
+            CulcArea(a2, b2, c2, out S2);
+            Console.WriteLine("Площадь второго треугольника равна: {0:##########.00}", S2);
+
+            if (S1>S2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            else
+            {
+                if (S1 < S2)
+                {
+                    Console.WriteLine("Площадь второго треугольника больше");
+                }
+                else
+                {
+                    Console.WriteLine("Площади треугольников равны");
+                }
+            }
+            
+            Console.ReadKey();
+        }
+        static void CulcArea(int a, int b, int c, out double S)
+        {
+            double p = ((double)a + b + c) / 2;
+            S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+    }
+}
---
+++ Lab7_Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double S;
+            double V;
+            CulcCube(a, out S, out V);
+            Console.WriteLine("Площадь куба равна {0:##########,00}", S);
+            Console.WriteLine("Объем куба равен {0:#######,00}", V);
+            Console.ReadKey();
+        }
+        static void CulcCube(double a, out double S, out double V)
+        {
+            S = 6 * Math.Pow(a, 2);
+            V = Math.Pow(a, 3);
+        }
+
+    }
+}
